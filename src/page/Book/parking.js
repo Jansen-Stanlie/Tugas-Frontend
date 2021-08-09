1bf0b17e5913aeb4352691fd91a00af17a3d8da4
Jansen-Stanlie <jansenstan24@gmail.com>
2021-08-09 13:07:43 +0700

    perbaikan

--- src/page/Book/parking.js
+++ src/page/Book/parking.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { FirebaseContext } from "../../config/firebase";
 
 class ParkingPageFirebase extends Component {
